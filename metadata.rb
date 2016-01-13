3888e222049f46fe4c6d7458bc2c0981c48e0dff
Thomas Boerger <tboerger@suse.de>
2016-01-13 09:37:38 +0100

    Initial commit

---
+++ metadata.rb
@@ -0,0 +1,13 @@
+name "sound-juicer"
+maintainer "Thomas Boerger"
+maintainer_email "tboerger@tbpro.de"
+license "Apache 2.0"
+description "Installs/Configures sound-juicer"
+long_description IO.read(File.join(File.dirname(__FILE__), "README.md"))
+version "0.0.1"
+depends "zypper"
+recipe "sound-juicer", "Installs/Configures sound-juicer"
+
+supports "suse", ">= 12.3"
+supports "debian", ">= 7.0"
+supports "ubuntu", ">= 12.04"
